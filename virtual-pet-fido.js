4d492782fd0a053f31a7729f2742e140622a9980
Joanna Hughes <joannahughes@hotmail.co.uk>
2021-01-17 13:23:34 +0000

    commit to main

--- virtual-pet-fido.js
+++ virtual-pet-fido.js
@@ -7,12 +7,12 @@ function pet(name, age, alive){
     this.fitness = 10,
     this.walk = 4,
     this.isAlive = alive
-},
+};
 
 pet.prototype = {
     growUp() {
     this.age += 1;
-},
+};
 
 petHungry(hungerIncrease) {
 
@@ -20,14 +20,14 @@ petHungry(hungerIncrease) {
     this.hunger.push(5);
     } else {
     this.hunger.push()
-}},
+}};
 
 lessFit() {
     if (pet.growUp === true) {
     this.fitness -= 3;
     } else {
     this.walk 
-}},
+}};
 
 walkFit() {
  if (pet.this.fitness + 4) <= 10 ) {
@@ -36,14 +36,14 @@ this.fitness += 4;
 this.fitness = 10;
     }
       }
-},
+};
 
 feedPet() {
     if (pet.this.feedPet <= 10 ) {
     this.feedPet += 3;
     } else {
     this.feedPet = 0;
-},
+};
 
 checkUp() {
     if (pet.this.fitness === 3 && pet.this.fitness <= 3) {
@@ -61,11 +61,11 @@ checkUp() {
     if (pet.this.isAlive) {
         throw new Error('Your pet is no longer alive :(')
           }
-      },
+      };
     
 isAlive() 
  return this.age < 30 && this.hunger < 10 && this.fitness > 0;
  }
-},
+};
 
 module.exports = {pet};
